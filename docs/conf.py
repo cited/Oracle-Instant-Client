b910f8fe69f0ae484268dd8c7dfeb52e7d8dce59
David Ghedini <david@davidghedini.com>
2023-08-07 07:52:46 +0300

    Update

--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Oracle Instant Client'
-copyright = u'2023, Cited, Inc.'
+project = u'Tomcat Webmin Module'
+copyright = u'2020, Cited, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -98,7 +98,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
